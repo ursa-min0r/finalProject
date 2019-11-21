5a1c567309e749558ac62bb683e5a057ab04a13c
Polaris <tristynkcline@gmail.com>
2019-11-21 11:08:19 -0600

    Log In Update

--- sourceFiles/src/views/Login.js
+++ sourceFiles/src/views/Login.js
@@ -16,22 +16,21 @@ class Login extends React.Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-10 col-xl-7 mx-auto">
-                            <h3 className="display-4">Split page!</h3>
-                            <p className="text-muted mb-4">Create a login split page using Bootstrap 4.</p>
+                            <img src={require("../../src/images/scriptHub.png")} alt="Header" width="300px" height="300px"/>
+                            
                             <form>
                                 <div className="form-group mb-3">
                                     <input id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" className="form-control rounded-pill border-0 shadow-sm px-4"></input>
                                 </div>
                                 <div className="form-group mb-3">
-                                    <input id="inputPassword" type="password" placeholder="Password" required="" className="form-control rounded-pill border-0 shadow-sm px-4 text-primary"></input>
+                                    <input id="inputPassword" type="password" placeholder="Password" required="" className="form-control rounded-pill border-0 shadow-sm px-4 text-dark"></input>
                                 </div>
                                 <div className="custom-control custom-checkbox mb-3">
                                     <input id="customCheck1" type="checkbox" checked className="custom-control-input"></input>
-                                    <label for="customCheck1" className="custom-control-label">Remember password</label>
+                                    <label for="customCheck1" className="custom-control-label">Remember Password?</label>
                                 </div>
-                                <button type="submit" className="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
-                                <div className="text-center d-flex justify-content-between mt-4"><p>Snippet by <a href="https://bootstrapious.com/snippets" className="font-italic text-muted"> 
-                                        <u>Boostrapious</u></a></p></div>
+                                <button type="submit" className="btn btn-dark btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
+                                
                             </form>
                         </div>
                     </div>
