3713d164918381035dc52b8344d55abfc877d6dc
Lowden Hall <53575153+Mozzer2323@users.noreply.github.com>
2019-11-21 11:59:50 -0600

    Changed navbar colors, fixed dashboard container.

--- sourceFiles/src/views/Dashboard.js
+++ sourceFiles/src/views/Dashboard.js
@@ -5,91 +5,88 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className="dashcontain">
-        
         <div className="pagehead">
           <h1>What's New</h1>
         </div>
-
-          <div className="meetupswrap">
-            <div className="meetupheader">
-              <p>/MEETUPS AND EVENTS</p>
-            </div>
-              <div className="meetupcontent">
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-              </div>
+        <div className="meetupswrap">
+          <div className="meetupheader">
+            <p>/MEETUPS AND EVENTS</p>
           </div>
-
-          <div className="newswrap">
-            <div className="newsheader">
-              <p>/NEWS</p>
+          <div className="meetupcontent">
+            <div className="box">
+              <p>Item</p>
             </div>
-              <div className="newscontent">
-              <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-              </div>
-          </div>
-
-          <div className="newswrap">
-            <div className="newsheader">
-              <p>/FORUM</p>
+            <div className="box">
+              <p>Item</p>
             </div>
-              <div className="newscontent">
-              <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-              </div>
-          </div>
-
-          <div className="newswrap">
-            <div className="newsheader">
-              <p>/RECOMMENDED TUTORIALS</p>
+            <div className="box">
+              <p>Item</p>
             </div>
-              <div className="newscontent">
-              <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-                <div className="box">
-                <p>Item</p>
-                </div>
-              </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
           </div>
-    </div>
+        </div>
+        <div className="newswrap">
+          <div className="newsheader">
+            <p>/NEWS</p>
+          </div>
+          <div className="newscontent">
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="newswrap">
+          <div className="newsheader">
+            <p>/FORUM</p>
+          </div>
+          <div className="newscontent">
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="newswrap">
+          <div className="newsheader">
+            <p>/RECOMMENDED TUTORIALS</p>
+          </div>
+          <div className="newscontent">
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+            <div className="box">
+              <p>Item</p>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
