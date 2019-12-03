d0f30a1f90193514e8476179c0fc513d6c03e73e
Polaris <tristynkcline@gmail.com>
2019-12-02 18:28:01 -0600

    Removed Dashboard Components

--- sourceFiles/src/views/Dashboard.js
+++ sourceFiles/src/views/Dashboard.js
@@ -2,60 +2,57 @@ import React from "react";
 import { Card, CardBody, } from "shards-react";
 
 import "../views/dashcomponents/dash.css"
-import Meetups from "./Meetups";
-import Community from "./Community";
-import Tutorials from "./Tutorials";
+
 
 
 class Dashboard extends React.Component {
   render() {
     return (
       <Card className="dashcontain">
-
-        <Card className="meetupswrap">
-          <CardBody className="meetupheader">
-            <div className="imageButton">
-              <a href="http://localhost:3000/meetups">
-              <img
-                src={require("../../src/images/2.png")} alt="Header" width="1000px" height="100px" className="center" />
-           </a>  
-           <Meetups />
-            </div>
-           
-             
-               
-         
-          </CardBody>
-        </Card>
+        <CardBody className="dashwrapper">
 
 
-        <Card className="forumwrap">
-          <CardBody className="forumheader">
-            <div className="imageButton">
-              <a href="http://localhost:3000/add-new-post">
-              <img
-                src={require("../../src/images/3.png")} alt="Header" width="1000px" height="100px" className="center" />
-            </a>
-            <Community />
-            </div>
-        
-          </CardBody>
-        </Card>
+          <center>
+            <img
+            src={require("../../src/images/loginimagefix.PNG")} alt="Header" width="1003px" height="871px" align="center" 
+            />
+          </center>
 
 
-        <Card className="tutorwrap">
-          <CardBody className="tutorheader">
-            <div className="imageButton">
-              <a href="http://localhost:3000/tutorials">
-              <img
-                src={require("../../src/images/4.png")} alt="Header" width="1000px" height="100px" className="center" />
-           </a> 
-           <Tutorials />
-           </div>
-     
-          </CardBody>
-        </Card>
+
+
+
+        </CardBody>
       </Card>
+
+
+      /*<Card className="forumwrap">
+        <CardBody className="forumheader">
+          <div className="imageButton">
+            <a href="http://localhost:3000/add-new-post">
+            <img
+              src={require("../../src/images/3.png")} alt="Header" width="1000px" height="100px" className="center" />
+          </a>
+          <Community />
+          </div>
+      
+        </CardBody>
+      </Card>
+
+
+      <Card className="tutorwrap">
+        <CardBody className="tutorheader">
+          <div className="imageButton">
+            <a href="http://localhost:3000/tutorials">
+            <img
+              src={require("../../src/images/4.png")} alt="Header" width="1000px" height="100px" className="center" />
+         </a> 
+         <Tutorials />
+         </div>
+   
+        </CardBody>
+      </Card>
+    </Card>*/
     )
   }
 }
